23de38033c171c9a81afcef0def094dbc8bba0c9
Ankita Khera <ankitakhera@microsoft.com>
2021-08-31 14:25:14 -0700

    need to figure out how to deal with adornment not taking up a character

---
+++ src/VisualStudio/IntegrationTest/IntegrationTests/CSharp/CSharpInlineHints.cs
@@ -0,0 +1,54 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Editor.Shared.Options;
+using Microsoft.CodeAnalysis.InlineHints;
+using Microsoft.CodeAnalysis.Test.Utilities;
+using Microsoft.VisualStudio.IntegrationTest.Utilities;
+using Roslyn.Test.Utilities;
+using Xunit;
+
+namespace Roslyn.VisualStudio.IntegrationTests.CSharp
+{
+    [Collection(nameof(SharedIntegrationHostFixture))]
+    public class CSharpInlineHints : AbstractEditorTest
+    {
+        protected override string LanguageName => LanguageNames.CSharp;
+
+        public CSharpInlineHints(VisualStudioInstanceFactory instanceFactory)
+            : base(instanceFactory, nameof(CSharpInlineHints))
+        {
+        }
+
+        [WpfFact, Trait(Traits.Feature, Traits.Features.InlineHints)]
+        public void QuickInfo_Inline_Hint_Simple_Case()
+        {
+            SetUpEditor(@"
+using System;
+public class Program
+{
+    public int Method(int a, int b)
+    {
+        int result = a * b;
+        return result;
+    }
+
+    public void CallingMethod()
+    {
+        _ = Method($$5, 10);
+    }
+}");
+            VisualStudio.Workspace.SetFeatureOption(InlineHintsOptions.EnabledForParameters.Feature, InlineHintsOptions.EnabledForParameters.Name, LanguageName, "True");
+            //VisualStudio.Editor.PlaceCaret("5", charsOffset: -1);
+            VisualStudio.Editor.InvokeQuickInfo();
+            Assert.Equal("class Program\r\nHello!", VisualStudio.Editor.GetQuickInfo());
+        }
+    }
+}
