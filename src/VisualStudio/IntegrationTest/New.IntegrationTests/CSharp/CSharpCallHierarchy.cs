bd13d7b7f8094b1ab96ef1afbd4dabd8c11733aa
akhera99 <ankitakhera@microsoft.com>
2022-12-14 15:46:45 -0800

    wip

---
+++ src/VisualStudio/IntegrationTest/New.IntegrationTests/CSharp/CSharpCallHierarchy.cs
@@ -0,0 +1,72 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Threading.Tasks;
+using System;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Test.Utilities;
+using Microsoft.VisualStudio.Shell.TableControl;
+using Roslyn.VisualStudio.IntegrationTests;
+using WindowsInput.Native;
+using Xunit;
+using Microsoft.VisualStudio.Shell.TableManager;
+
+namespace Roslyn.VisualStudio.NewIntegrationTests.CSharp
+{
+    [Trait(Traits.Feature, Traits.Features.CallHierarchy)]
+    public class CSharpCallHierarchy : AbstractEditorTest
+    {
+        public CSharpCallHierarchy()
+            : base(nameof(CSharpCallHierarchy))
+        {
+        }
+
+        protected override string LanguageName => LanguageNames.CSharp;
+
+        [IdeFact]
+        public async Task CallHierarchyToCtor()
+        {
+            await SetUpEditorAsync(@"
+class Program
+{
+}$$
+", HangMitigatingCancellationToken);
+            await TestServices.SolutionExplorer.AddFileAsync(ProjectName, "File2.cs", cancellationToken: HangMitigatingCancellationToken);
+            await TestServices.SolutionExplorer.OpenFileAsync(ProjectName, "File2.cs", HangMitigatingCancellationToken);
+
+            await SetUpEditorAsync(@"
+class SomeOtherClass
+{
+    void M()
+    {
+        Program p = new Progr$$am();
+    }
+}
+", HangMitigatingCancellationToken);
+
+            await TestServices.Input.SendAsync((VirtualKeyCode.CONTROL, VirtualKeyCode.VK_K), HangMitigatingCancellationToken);
+            await TestServices.Input.SendAsync((VirtualKeyCode.CONTROL, VirtualKeyCode.VK_T), HangMitigatingCancellationToken);
+
+            var results = await TestServices.CallHierarchyWindow.GetContentsAsync(HangMitigatingCancellationToken);
+
+            Assert.Collection(
+                results,
+                new Action<ITableEntryHandle2>[]
+                {
+                    reference =>
+                    {
+                        Assert.Equal(expected: "class Program", actual: reference.TryGetValue(StandardTableKeyNames.Text, out string code) ? code : null);
+                        Assert.Equal(expected: 1, actual: reference.TryGetValue(StandardTableKeyNames.Line, out int line) ? line : -1);
+                        Assert.Equal(expected: 6, actual: reference.TryGetValue(StandardTableKeyNames.Column, out int column) ? column : -1);
+                    },
+                    reference =>
+                    {
+                        Assert.Equal(expected: "Program p = new Program();", actual: reference.TryGetValue(StandardTableKeyNames.Text, out string code) ? code : null);
+                        Assert.Equal(expected: 5, actual: reference.TryGetValue(StandardTableKeyNames.Line, out int line) ? line : -1);
+                        Assert.Equal(expected: 24, actual: reference.TryGetValue(StandardTableKeyNames.Column, out int column) ? column : -1);
+                    }
+                });
+        }
+    }
+}
---
+++ src/VisualStudio/IntegrationTest/New.IntegrationTests/InProcess/CallHierarchyWindowInProcess.cs
@@ -0,0 +1,81 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.Shared.TestHooks;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Extensibility.Testing;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.VisualStudio.Shell.TableControl;
+using Microsoft.VisualStudio.Threading;
+
+namespace Roslyn.VisualStudio.NewIntegrationTests.InProcess
+{
+    [TestService]
+    internal partial class CallHierarchyWindowInProcess
+    {
+        internal static readonly Guid ViewCallHierarchyGuid = new("{68487888-204A-11D3-87EB-00C04F7971A5}");
+
+        public async Task<ImmutableArray<ITableEntryHandle2>> GetContentsAsync(CancellationToken cancellationToken)
+        {
+            await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+
+            await TestServices.Workspace.WaitForAsyncOperationsAsync(FeatureAttribute.CallHierarchy, cancellationToken);
+
+            // Find the tool window
+            var tableControl = await GetCallHierarchyWindowAsync(cancellationToken);
+
+            // Remove all grouping
+            var columnStates = tableControl.ColumnStates;
+            var newColumnsStates = new List<ColumnState2>();
+            foreach (ColumnState2 state in columnStates)
+            {
+                var newState = new ColumnState2(
+                    state.Name,
+                    state.IsVisible,
+                    state.Width,
+                    state.SortPriority,
+                    state.DescendingSort,
+                    groupingPriority: 0);
+                newColumnsStates.Add(newState);
+            }
+
+            tableControl.SetColumnStates(newColumnsStates);
+
+            // Force a refresh, if necessary. This doesn't re-run the Find References or
+            // Find Implementations operation itself, it just forces the results to be
+            // realized in the table.
+            var forcedUpdateResult = await tableControl.ForceUpdateAsync().WithCancellation(cancellationToken);
+
+            // Extract the basic text of the results.
+            return forcedUpdateResult.AllEntries.Cast<ITableEntryHandle2>().ToImmutableArray();
+        }
+
+        private async Task<IWpfTableControl2> GetCallHierarchyWindowAsync(CancellationToken cancellationToken)
+        {
+            await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+
+            var shell = await GetRequiredGlobalServiceAsync<SVsUIShell, IVsUIShell>(cancellationToken);
+            ErrorHandler.ThrowOnFailure(shell.FindToolWindowEx((uint)__VSFINDTOOLWIN.FTW_fFindFirst, ViewCallHierarchyGuid, dwToolWinId: 0, out var windowFrame));
+            ErrorHandler.ThrowOnFailure(windowFrame.GetProperty((int)__VSFPROPID.VSFPROPID_DocView, out var toolWindow));
+
+            // Dig through to get the Find References control.
+            var toolWindowType = toolWindow.GetType();
+            var toolWindowControlField = toolWindowType.GetField("Control");
+            var toolWindowControl = toolWindowControlField.GetValue(toolWindow);
+
+            // Dig further to get the results table (as opposed to the toolbar).
+            var tableControlAndCommandTargetType = toolWindowControl.GetType();
+            var tableControlField = tableControlAndCommandTargetType.GetField("TableControl");
+            var tableControl = (IWpfTableControl2)tableControlField.GetValue(toolWindowControl);
+            return tableControl;
+        }
+    }
+}
