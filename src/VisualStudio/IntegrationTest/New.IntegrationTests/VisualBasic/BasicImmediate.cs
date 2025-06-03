e6b1e48008a485299e673eaa7281477c0b9b4ace
akhera99 <ankitakhera@microsoft.com>
2025-06-03 13:27:15 -0700

    skip tests

--- src/VisualStudio/IntegrationTest/New.IntegrationTests/VisualBasic/BasicImmediate.cs
+++ src/VisualStudio/IntegrationTest/New.IntegrationTests/VisualBasic/BasicImmediate.cs
@@ -29,7 +29,7 @@ public class BasicImmediate : AbstractEditorTest
         await TestServices.SolutionExplorer.AddProjectAsync(ProjectName, WellKnownProjectTemplates.ConsoleApplication, LanguageNames.VisualBasic, HangMitigatingCancellationToken);
     }
 
-    [IdeFact]
+    [IdeFact(Skip = "")]
     public async Task DumpLocalVariableValue()
     {
         await TestServices.Editor.SetTextAsync(@"
